b753bf35785bd44928fca8344a0571e7832a7b57
BeomyeonAndrewKim <beom911@gmail.com>
2019-02-11 22:51:22 +0900

    feat: Chart 컴포넌트 생성 및 라우터 설정

---
+++ src/components/Chart.vue
@@ -0,0 +1,14 @@
+
+<script>
+import { PolarArea } from "vue-chartjs";
+export default {
+  extends: PolarArea,
+  props: ["chartdata", "options"],
+  mounted() {
+    this.renderChart(this.chartdata, this.option);
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
