04c7fee2163c2c355d70d834b3e3eea6fd3e86c7
Kim Bum June <bjbj6363@gmail.com>
2019-02-15 11:29:37 +0900

    update: 스크롤 시 선 올라가는 이벤트 추가

--- src/components/common/CountComp.vue
+++ src/components/common/CountComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="count-comp">
+  <div ref="anime" class="count-comp">
     <div class="count-title">Counting</div>
     <span>{{count}}</span>
   </div>
@@ -9,6 +9,19 @@
 export default {
   created() {
     this.$store.dispatch("fetchCount");
+  },
+  mounted() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add("move");
+          return;
+        }
+        entry.target.classList.remove("move");
+      });
+    });
+
+    observer.observe(this.$refs["anime"]);
   },
   computed: {
     count() {
