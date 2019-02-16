96464543573b6b27e87587ed7ff740b3ad493947
YoonYIgeon <whaledoll@naver.com>
2019-02-16 15:11:36 +0900

    반응형 업데이트

--- src/components/common/CountComp.vue
+++ src/components/common/CountComp.vue
@@ -1,35 +1,35 @@
 <template>
   <div ref="anime" class="count-comp">
-    <div class="count-title">Counting</div>
-    <span>{{count}}</span>
+    <div class="container">
+      <div class="count-title">Counting</div>
+      <span>{{count}}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   created() {
-    this.$store.dispatch("fetchCount");
+    this.$store.dispatch("fetchCount")
   },
   mounted() {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
-          entry.target.classList.add("move");
-          return;
+          entry.target.classList.add("move")
+          return
         }
-        entry.target.classList.remove("move");
-      });
-    });
+        entry.target.classList.remove("move")
+      })
+    })
 
-    observer.observe(this.$refs["anime"]);
+    observer.observe(this.$refs["anime"])
   },
   computed: {
     count() {
-      return this.$store.getters.count;
+      return this.$store.getters.count
     }
   }
-};
+}
 </script>
 
-<style>
-</style>
