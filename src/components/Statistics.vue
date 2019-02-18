c1b53241d2eb9755b3d248d9b1bba4c37898c545
BeomyeonKim <bykim@nomadconnection.com>
2019-02-18 21:36:17 +0900

    fix: Chart update 버그 수정 및 옵션 및 데이터 부모 컴포넌트로 이동

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -5,10 +5,78 @@
         <div class="statistics-wrapper">
           <statistics-header></statistics-header>
           <div class="chart-wrapper">
-            <chart></chart>
+            <chart :chartData="chartData" :options="options"></chart>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    chartData() {
+      return {
+        labels: this.$store.getters.chartLabels,
+        datasets: [
+          {
+            backgroundColor: "rgba(57, 53, 119, 0.7)",
+            data: this.$store.getters.chartData
+          }
+        ]
+      };
+    },
+    options() {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: "#979797"
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Count"
+              }
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: "#979797"
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Days"
+              }
+            }
+          ]
+        },
+        legend: {
+          display: false
+        },
+        layout: {
+          padding: {
+            left: 60,
+            right: 120,
+            top: 0,
+            bottom: 0
+          }
+        },
+        tooltips: {
+          mode: "label"
+        }
+      };
+    }
+  },
+  created() {
+    const { url } = this.$route.params;
+    this.$store.dispatch("GET_URL_COUNT_BY_WEEK", { url, week: 1 });
+  }
+};
+</script>
--- src/components/common/Chart.vue
+++ src/components/common/Chart.vue
@@ -1,88 +1,21 @@
 
 <script>
-import { Line, mixins } from "vue-chartjs"
+import { Line, mixins } from "vue-chartjs";
 export default {
   extends: Line,
   mixins: [mixins.reactiveProp],
-  computed: {
-    urlCountByWeek() {
-      return this.$store.getters.urlCountByWeek
+  props: {
+    chartData: {
+      type: Object,
+      required: false
     },
-    // 기존에 두개로 나눠두신 created를 하나로 합쳤어여
-    // renderChart 첫번째 인자
-    chartData() {
-      return {
-        labels: this.$store.getters.chartLabels,
-        datasets: [
-          {
-            backgroundColor: "rgba(57, 53, 119, 0.7)",
-            data: this.$store.getters.chartData
-          }
-        ]
-      }
+    options: {
+      type: Object,
+      required: false
     }
-  },
-  created() {
-    const { url } = this.$route.params
-    this.$store.dispatch("GET_URL_COUNT_BY_WEEK", { url, week: 1 }).then(data => {
-      console.log("disaptched in component created cycle")
-      console.log(data)
-    })
   },
   mounted() {
-    // const { labels, data } = this.urlCountByWeek;
-    console.log("mounted component", this.chartLabels, this.chartData)
-    this.renderChart(this.chartData, this.options)
-  },
-  data() {
-    return {
-      // renderChart 두 번째 인자
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: "#979797"
-              },
-              scaleLabel: {
-                display: true,
-                labelString: "Count"
-              }
-            }
-          ],
-          xAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: "#979797"
-              },
-              scaleLabel: {
-                display: true,
-                labelString: "Days"
-              }
-            }
-          ]
-        },
-        legend: {
-          display: false
-        },
-        layout: {
-          padding: {
-            left: 60,
-            right: 120,
-            top: 0,
-            bottom: 0
-          }
-        },
-        tooltips: {
-          mode: "label"
-        }
-      }
-    }
+    this.renderChart(this.chartData, this.options);
   }
-}
-
+};
 </script>
