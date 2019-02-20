621d90d61cad92047902db15ff25108249a1ac28
YoonYIgeon <whaledoll@naver.com>
2019-02-21 01:14:34 +0900

    update: table component

---
+++ src/components/common/ChartTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <table class='chart'>
+    <thead>
+      <tr>
+        <th>Page</th>
+        <th>Views</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="i in 30" :key='i'>
+        <td>/?fbclid=kwARo-FB&Hello=World</td>
+        <td>3,222</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+  export default {}
+</script>
+<style>
+</style>
